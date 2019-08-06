78b924f77257aa22eb78708cc3640f565b7501e1
Seth Nadu <seth.nadu@gmail.com>
2019-08-06 15:27:24 -0400

    fetch info/ set state

--- githubcards/src/App.js
+++ githubcards/src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// https://api.github.com/users/sethnadu
+
+
+
+
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        user: [],
+        followers: []
+      }
+    }
+
+    fetchInfo = Promise.all([
+      fetch("https://api.github.com/users/sethnadu").then(response => response.json()),
+      fetch("https://api.github.com/users/sethnadu/followers").then(response => response.json())
+    ])
+      .then(([userGithub, userFollowers]) => {
+          this.setState({user: userGithub})
+          this.setState({followers: userFollowers})
+      })
+
+
+  render() {
+    console.log(this.state.user)
+    console.log(this.state.followers)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+ } 
 }
 
 export default App;
