263e78e070358b0345fcbf2b9533bc39594e5246
Seth Nadu <seth.nadu@gmail.com>
2019-08-06 16:59:28 -0400

    working on followers cards

--- githubcards/src/App.js
+++ githubcards/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import UserCard from "./Component/user.js";
 import './App.css';
 
 // https://api.github.com/users/sethnadu
@@ -12,26 +12,41 @@ class App extends React.Component {
       super();
       this.state = {
         user: [],
-        followers: []
+        followers: [],
+        followersNames: []
       }
     }
 
-    fetchInfo = Promise.all([
+    componentDidMount() {
+      this.fetchInfo();
+    }
+
+
+
+    fetchInfo = () => Promise.all([
       fetch("https://api.github.com/users/sethnadu").then(response => response.json()),
       fetch("https://api.github.com/users/sethnadu/followers").then(response => response.json())
     ])
       .then(([userGithub, userFollowers]) => {
           this.setState({user: userGithub})
           this.setState({followers: userFollowers})
+          this.setState({followersNames: userFollowers.map(follower => {
+            return follower.login
+          })})
       })
 
 
   render() {
     console.log(this.state.user)
     console.log(this.state.followers)
+    console.log(this.state.followersNames)
   return (
     <div className="App">
-      
+        <h2>Github User Cards</h2>
+        <div>
+        <UserCard user = {this.state.user} followers = {this.state.followers} />
+        {/* {this.state.followersNames.map()} */}
+        </div>
     </div>
   );
  } 
---
+++ githubcards/src/Component/user.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+import GithubIcon from "../Assets/githublogo.png";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: "white",
+
+  },
+  githubLogo: {
+      width: "43px"
+  }
+}));
+
+function UserCard({user, followers}) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    function handleExpandClick() {
+        setExpanded(!expanded);
+    }
+
+    return (
+        <div>
+            {/* <h2>{user.login}</h2>
+            <p>{user.name}</p>
+            <img src ={user.avatar_url} alt = {`${user.name}'s Avatar`} />
+            <p>{user.bio}</p>
+            <p>{user.public_repos}</p>
+            <p>{user.repos_url}</p>
+            <p>{user.following}</p>
+            <p>{user.followers}: </p>
+            {followers.map(follower => {
+              return <p key = {follower.id}>{follower.login}</p>
+            })} */}
+             <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {<img className={classes.githubLogo} alt = "github" src = {GithubIcon}/>}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+           
+          </IconButton>
+        }
+        title={user.login}
+        subheader={user.name}
+      />
+      <CardMedia
+        className={classes.media}
+        image={user.avatar_url}
+        title="Avatar Image"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {user.bio}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          
+        </IconButton>
+        <IconButton aria-label="share">
+         
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>GitHub Info</Typography>
+          <Typography paragraph>
+            Repos: {user.public_repos}
+            <br />
+            Repos Url : {user.repos_url}
+          </Typography>
+          <Typography paragraph>
+            Following: {user.following}
+            <br />
+            Followers: {user.followers}
+            <br />
+            Followers: 
+            
+            {followers.map(follower => {
+              return (
+                     <li key = {follower.id}>{follower.login}</li>
+              ) 
+            })}
+            
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+
+           
+
+        </div>
+    )
+}
+
+export default UserCard;
